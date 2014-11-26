1ea416920a2e5e5605ae03ddd2bef785a3821939
Michael Partridge <partridgems@gmail.com>
2014-11-26 12:20:49 -0500

    Packaging!

---
+++ dbms/database/BHDbms.java
@@ -0,0 +1,35 @@
+package database;
+
+
+public class BHDbms{
+	
+	/**
+	 *	Constructs partial ordering of the tables.
+	 *
+	 */
+
+	@Override
+	protected void constructDependencies(){
+		paths = new HashMap<Table, List<Table>>();
+		for(int i = 0; i < tables.size; i++){
+			paths.put(tables[i], getPath(i));
+		}
+	}
+
+	/**
+	 * Constructs path from root based on BH structure
+	 */
+	private List<Table> getPath(int start){
+		int currentIndex = start;
+		Table currentTable;
+		ArrayList<Table> path = new ArrayList<Table>();
+		while(currentIndex >= 0){
+			currentTable = tables[currentIndex];
+			path.add(0,currentTable);
+			currentIndex = (currentIndex - 1) / 2;
+		}
+
+		return path;
+
+	}
+}
---
+++ dbms/lockManagement/LockRleaser.java
@@ -0,0 +1,83 @@
+package lockManagement;
+import database.Table;
+
+/*
+ * LockReleaser will be spawned by a Transaction and
+ * is in charge of releasing unnecessary locks.
+ *
+ * Author: Nicholas Cummins
+ * email: ncummins@brandeis.edu
+ * 10/26/14
+ */
+
+public abstract class LockReleaser{
+
+	/**
+	 *	Creates a new LockReleaser
+	 *
+	 *	@param the int id of the parent transaction
+	 *	@param the List<List<Table>> of dependencies
+	 *	@param the BlockingQueue used to communicate
+	 *
+	 */
+
+	public LockReleaser(int id, List<List<Table>> dependencies, 
+									BlockingQueue<Table> unnecessaryLocks){
+		this.id = id;
+		this.dependencies = dependencies;
+		this.unnecessaryLocks = unnecessaryLocks;
+		this.initMap();
+	}
+
+	/**
+	 * Initializes the dependency map so the releaser
+	 * can keep track of what locks are ready to be 
+	 * released
+	 *
+	 */
+
+	private void initMap(){
+		dependecyMap = new HashMap<Table>();
+		for(List<Table> list : dependencies){
+			for(Table table : list){
+				if((value = dependecyMap.get(table)) == null){
+					dependecyMap.put(table, 1);
+				} else {
+					dependecyMap.put(table, value + 1);
+				}
+			}
+		}
+
+	}
+
+	/**
+	 *	Monitors the unnecessaryLocks Queue and
+	 * 	decrements the count of any locks placed
+	 *	there.  When a lock is no longer needed
+	 *	at all it will be released. 
+	 *
+	 */
+
+	public final void run(){
+		while(true){
+			Table lock = unnecessaryLocks.take();
+			if((value = dependecyMap.get(lock)) == null){
+				//Throw an exception
+			} else if (value <= 1){
+				dependecyMap.remove(lock);
+				lock.releaseLock(id);
+				if(dependecyMap.isEmpty()){
+					return; //No more dependencies, finished.
+				}
+			} else {
+				dependecyMap.put(lock, value - 1);
+			}
+		}
+	}
+
+
+	private int id;
+	private BlockingQueue<Table> unnecessaryLocks;
+	private HashMap<Table, Integer> dependecyMap;
+	private List<List<Table>> dependencies;
+}
